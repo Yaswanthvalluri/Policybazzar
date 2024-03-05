f10ebe261d039fa909275b081919093612e36971
yaswanth <valluriyaswanth@gmail.com>
2024-03-05 18:23:30 +0530

    commit2

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 //@RunWith(Cucumber.class)
-	@CucumberOptions(features={".//FeatureFile/policybazzar-regression.feature"},
+	@CucumberOptions(features={".//FeatureFile/policybazzar.feature"},
 	glue= {"com.Policybazzar.StepDefination"},
 	plugin= {"pretty", "html:reports/myreportss.html",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
